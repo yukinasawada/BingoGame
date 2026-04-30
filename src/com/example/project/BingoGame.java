f77ef4510e694a78fd6e9ccad61fc880d57a99be
yukinasawada <yukina.sawada366@gmail.com>
2026-04-30 17:39:57 +0900

    createPickNumber()、定数row追加
    Merge branch 'master' of https://github.com/yukinasawada/BingoGame.git

--- src/com/example/project/BingoGame.java
+++ src/com/example/project/BingoGame.java
@@ -2,41 +2,37 @@ package com.example.project;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 class BingoGame {
 
+	final int row = 7;
+	final int center = row / 2;
+
 	void start() {
 		//ビンゴカードとビンゴカードチェックを初期化
-		int[][] bingoCard = new int[5][5];
-		boolean[][] checkOpened = new boolean[5][5];
+		int[][] bingoCard = new int[row][row];
+		boolean[][] checkOpened = new boolean[row][row];
 		int pickNumber = 0;
 
 		//FREEに予めtureを入れておく
-		checkOpened[2][2] = true;
+		checkOpened[center][center] = true;
 
 		//ランダムなビンゴカードを作成		  
 		bingoCard = creatBingoCard();
 
-		//ランダムで1～75を引く
-		ArrayList<Integer> pickList = new ArrayList<>();
+		//ランダムな1～105の数字のリストを作成
+		List<Integer> pickList = createPickNumber();
 
-		//pickListに順番に格納
-		for (int i = 1; i <= 75; i++) {
-			pickList.add(i);
-		}
-
-		//pickListをシャッフル
-		Collections.shuffle(pickList);
-
-		//出たボールとカードの状態を表示する(75回施行)
-		for (int pickCount = 1; pickCount <= 75; pickCount++) {
+		//出たボールとカードの状態を表示する(105回施行)
+		for (int pickCount = 1; pickCount <= 105; pickCount++) {
 
 			pickNumber = pickList.get(pickCount - 1);
 			System.out.println("ball[" + pickCount + "]:" + pickNumber);
 
 			//出たボールがカード内にあればcheckOpenedをtrueにする
-			for (int i = 0; i < 5; i++) {
-				for (int j = 0; j < 5; j++) {
+			for (int i = 0; i < row; i++) {
+				for (int j = 0; j < row; j++) {
 					//pickNumberと一致したときTrueにする
 					if (bingoCard[i][j] == pickNumber) {
 						checkOpened[i][j] = true;
@@ -58,12 +54,27 @@ class BingoGame {
 		}
 	}
 
+	List<Integer> createPickNumber() {
+		//ランダムで1～105を引く
+		List<Integer> pickList = new ArrayList<>();
+
+		//pickListに順番に格納
+		for (int i = 1; i <= 105; i++) {
+			pickList.add(i);
+		}
+
+		//pickListをシャッフル
+		Collections.shuffle(pickList);
+
+		return pickList;
+	}
+
 	int[][] creatBingoCard() {
 
-		int[][] bingoCard = new int[5][5];
+		int[][] bingoCard = new int[row][row];
 		ArrayList<ArrayList<Integer>> lists = new ArrayList<ArrayList<Integer>>();
 
-		for (int j = 0; j < 5; j++) {
+		for (int j = 0; j < row; j++) {
 
 			ArrayList<Integer> list = new ArrayList<>();
 
@@ -77,7 +88,7 @@ class BingoGame {
 			lists.add(list);
 
 			//シャッフルした数字をビンゴカードに格納
-			for (int i = 0; i < 5; i++) {
+			for (int i = 0; i < row; i++) {
 				bingoCard[i][j] = lists.get(j).get(i);
 			}
 		}
@@ -87,62 +98,88 @@ class BingoGame {
 	int[] checkLines(boolean[][] checkOpened) {
 		int reach = 0;
 		int bingo = 0;
+		/* 上手く動かないのでコメントアウト。
+		int columnCount = 0;
+		
+		//横チェック・縦チェック
+		for (int i = 0; i < row; i++) {
+			//int count = 0;
+			int rowCount = 0;
+		
+			for (int j = 0; j < row; j++) {
+				if (checkOpened[i][j]) {
+					rowCount++;
+				} else if (checkOpened[j][i]) {
+					columnCount++;
+				}
+			}
+			if (rowCount == row && columnCount == row) {
+				bingo = bingo + 2;
+			} else if (rowCount == row || columnCount == row) {
+				bingo++;
+			} else if (rowCount == row - 1 && columnCount == row - 1) {
+				reach = reach + 2;
+			} else if (rowCount == row - 1 || columnCount == row - 1) {
+				reach++;
+			}
+		}
+		*/
 
 		//横チェック
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < row; i++) {
 			int count = 0;
 
-			for (int j = 0; j < 5; j++) {
-				if (checkOpened[i][j] == true) {
+			for (int j = 0; j < row; j++) {
+				if (checkOpened[i][j]) {
 					count++;
 				}
 			}
-			if (count == 5) {
+			if (count == row) {
 				bingo++;
-			} else if (count == 4) {
+			} else if (count == row - 1) {
 				reach++;
 			}
 		}
 
 		//縦チェック
-		for (int j = 0; j < 5; j++) {
+		for (int j = 0; j < row; j++) {
 			int count = 0;
 
-			for (int i = 0; i < 5; i++) {
-				if (checkOpened[i][j] == true) {
+			for (int i = 0; i < row; i++) {
+				if (checkOpened[i][j]) {
 					count++;
 				}
 			}
-			if (count == 5) {
+			if (count == row) {
 				bingo++;
-			} else if (count == 4) {
+			} else if (count == row - 1) {
 				reach++;
 			}
 		}
 
 		//斜めチェック(左上から右下)
 		int count = 0;
-		for (int i = 0; i < 5; i++) {
-			if (checkOpened[i][i] == true) {
+		for (int i = 0; i < row; i++) {
+			if (checkOpened[i][i]) {
 				count++;
 			}
 		}
-		if (count == 5) {
+		if (count == row) {
 			bingo++;
-		} else if (count == 4) {
+		} else if (count == row - 1) {
 			reach++;
 		}
 
 		//斜めチェック(右上から左下)
 		count = 0;
-		for (int i = 0; i < 5; i++) {
-			if (checkOpened[i][4 - i] == true) {
+		for (int i = 0; i < row; i++) {
+			if (checkOpened[i][row - 1 - i]) {
 				count++;
 			}
 		}
-		if (count == 5) {
+		if (count == row) {
 			bingo++;
-		} else if (count == 4) {
+		} else if (count == row - 1) {
 			reach++;
 		}
 
@@ -153,32 +190,21 @@ class BingoGame {
 
 	void showBingoCard(int[][] bingoCard, boolean[][] checkOpened) {
 
-		for (int i = 0; i < 5; i++) {
+		for (int i = 0; i < row; i++) {
 
-			for (int j = 0; j < 5; j++) {
+			for (int j = 0; j < row; j++) {
 
 				//真ん中はFREEを表示
-				if (i == 2 && j == 2) {
-					System.out.print("FREE");
+				if (i == center && j == center) {
+					System.out.print(" FREE");
 
 				} else if (checkOpened[i][j]) {
 					//10未満の場合は0を付けて表示
-					if (bingoCard[i][j] < 10) {
-						System.out.print("(0" + bingoCard[i][j] + ")");
-
-					} else {
-						System.out.print("(" + bingoCard[i][j] + ")");
-					}
+					System.out.printf("(%03d)", bingoCard[i][j]);
 
 				} else {
 					//10未満の場合は0を付けて表示
-					if (bingoCard[i][j] < 10) {
-						System.out.print(" 0" + bingoCard[i][j] + " ");
-
-					} else {
-						System.out.print(" " + bingoCard[i][j] + " ");
-
-					}
+					System.out.printf(" %03d ", bingoCard[i][j]);
 				}
 			}
 			//改行
